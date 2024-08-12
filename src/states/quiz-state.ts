46caf6d86df43fc5f4fb9526b7f631800880d807
seba53b4 <seba5364@hotmail.com>
2024-08-12 20:28:42 -0300

    persist added

--- src/states/quiz-state.ts
+++ src/states/quiz-state.ts
@@ -1,5 +1,5 @@
 import create from "zustand";
-import { devtools } from "zustand/middleware";
+import { persist } from "zustand/middleware";
 
 type QuestionState = {
   questionNumber: number;
@@ -24,58 +24,66 @@ type QuizState = {
 };
 
 export const useQuizStore = create<QuizState & QuizActions>()(
-  devtools((set) => ({
-    questionState: [],
-    questionQuizNumber: 1,
-    questionMaxNumber: 0,
-    isLoading: false,
-    addQuestionState: (questionState) =>
-      set((state) => ({
-        questionState: [...state.questionState, questionState],
-      })),
-    answerQuestion: (questionNumber, questionAnswer) =>
-      set((state) => {
-        const existingQuestion = state.questionState.find(
-          (q) => q.questionNumber === questionNumber
-        );
+  persist(
+    (set) => ({
+      questionState: [],
+      questionQuizNumber: 1,
+      questionMaxNumber: 0,
+      isLoading: false,
+      addQuestionState: (questionState) =>
+        set((state) => ({
+          questionState: [...state.questionState, questionState],
+        })),
+      answerQuestion: (questionNumber, questionAnswer) =>
+        set((state) => {
+          const existingQuestion = state.questionState.find(
+            (q) => q.questionNumber === questionNumber
+          );
 
-        if (existingQuestion) {
-          return {
-            questionState: state.questionState.map((q) =>
-              q.questionNumber === questionNumber ? { ...q, questionAnswer } : q
-            ),
-          };
-        } else {
-          return {
-            questionState: [
-              ...state.questionState,
-              { questionNumber, questionAnswer },
-            ],
-          };
-        }
-      }),
-    resetQuiz: () =>
-      set(() => ({
-        questionState: [],
-        questionQuizNumber: 1,
-        questionMaxNumber: 0,
-      })),
-    nextQuestion: () =>
-      set((state) => ({
-        questionQuizNumber: state.questionQuizNumber + 1,
-      })),
-    previousQuestion: () =>
-      set((state) => ({
-        questionQuizNumber: state.questionQuizNumber - 1,
-      })),
-    setQuestionMaxNumber: (maxNumber) =>
-      set(() => ({
-        questionMaxNumber: maxNumber,
-      })),
-    startQuiz: async () => {
-      set({ isLoading: true });
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      set({ isLoading: false });
-    },
-  }))
+          if (existingQuestion) {
+            return {
+              questionState: state.questionState.map((q) =>
+                q.questionNumber === questionNumber
+                  ? { ...q, questionAnswer }
+                  : q
+              ),
+            };
+          } else {
+            return {
+              questionState: [
+                ...state.questionState,
+                { questionNumber, questionAnswer },
+              ],
+            };
+          }
+        }),
+      resetQuiz: () =>
+        set(() => ({
+          questionState: [],
+          questionQuizNumber: 1,
+          questionMaxNumber: 0,
+        })),
+      nextQuestion: () =>
+        set((state) => ({
+          questionQuizNumber: state.questionQuizNumber + 1,
+        })),
+      previousQuestion: () =>
+        set((state) => ({
+          questionQuizNumber: state.questionQuizNumber - 1,
+        })),
+      setQuestionMaxNumber: (maxNumber) =>
+        set(() => ({
+          questionMaxNumber: maxNumber,
+        })),
+      startQuiz: async () => {
+        set({ isLoading: true });
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        set({ isLoading: false });
+      },
+    }),
+    {
+      name: "quiz-storage", // Nombre clave en localStorage
+      getStorage: () => localStorage, // O sessionStorage
+    }
+  )
 );
